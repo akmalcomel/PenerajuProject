313062eb44b33b28c18c33838ecdf9dbf7606555
akmal.arif <akmalarifarifin@gmail.com>
2024-06-24 21:04:10 +0800

    test first commit

---
+++ src/main/java/com/example/MasFlightBookingManagementSystem/MasFlightBookingManagementSystemApplication.java
@@ -0,0 +1,14 @@
+package com.example.MasFlightBookingManagementSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MasFlightBookingManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MasFlightBookingManagementSystemApplication.class, args);
+		//testing git
+	}
+
+}
